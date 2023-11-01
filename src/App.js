5f874a901ac618e0106aa3e6038789be10e1e827
Christopher Bryan <chrisbryann@Christophers-MacBook-Pro.local>
2023-11-01 16:25:10 -0700

    added hash router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Layout from "./components/Layout/Layout";
-import { Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import Schedule from "./components/Schedule/Schedule";
 import "./App.css";
 import ClassCard from "./components/UI/ClassCard";
@@ -10,11 +10,12 @@ function App() {
   return (
     <ClassesProvider>
       <Layout>
-        <Routes>
-          <Route path="/" element={<Schedule />} />
-          <Route path="/card" element={<ClassCard />} />
-          <Route path="*" element={<ErrorPage />} />
-        </Routes>
+        <HashRouter>
+          <Routes>
+            <Route path="/" element={<Schedule />} />
+            <Route path="*" element={<ErrorPage />} />
+          </Routes>
+        </HashRouter>
       </Layout>
     </ClassesProvider>
   );
